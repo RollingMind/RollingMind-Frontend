a9c62ca846eb89e9954e1b6c7cb7eabae45eb31f
MsEmily1020 <peace10200khs@gmail.com>
2024-04-23 14:23:12 +0900

    Fix :: home Login() 으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rollingmind_front/screens/login/login.dart';
 import 'package:rollingmind_front/screens/signup/signup.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(color: Color.fromRGBO(253, 146, 139, 1)),
-        ),
+  runApp(MaterialApp(
+    theme: ThemeData(
+      appBarTheme: AppBarTheme(
+        titleTextStyle: TextStyle(color: Color.fromRGBO(253, 146, 139, 1)),
       ),
-      home: SignUp(),
-    );
-  }
+    ),
+    home: Login(),
+  ));
 }

ab1a2678b94f5d6b6833a046dca95c7b399e74a2
MsEmily1020 <peace10200khs@gmail.com>
2024-04-25 16:35:39 +0900

    Design :: login - appbar 구성

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('로그인',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )),
+        centerTitle: true,
+        toolbarHeight: 80,
+        surfaceTintColor: Colors.white,
+        elevation: 10,
+        shadowColor: Colors.black,
+      ),
+    );
   }
 }

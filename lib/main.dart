8bd982b86d6a043c6cfcc7512bbb02f6bc4f6996
MsEmily1020 <peace10200khs@gmail.com>
2024-04-25 15:38:01 +0900

    Feat :: Splash 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rollingmind_front/screens/login/login.dart';
-import 'package:rollingmind_front/screens/signup/signup.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 3),
+  );
+  FlutterNativeSplash.remove();
+
   runApp(MaterialApp(
     theme: ThemeData(
       appBarTheme: AppBarTheme(

f30715ed06ca31f7d2ce4079528196df359256c8
MsEmily1020 <peace10200khs@gmail.com>
2024-04-26 01:11:01 +0900

    Design :: login - text 구성

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:rollingmind_front/utilities/colors.dart';
+import 'package:rollingmind_front/widgets/BaseAppBar.dart';
+import 'package:rollingmind_front/widgets/TextFieldArea.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('로그인',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            )),
-        centerTitle: true,
-        toolbarHeight: 80,
-        surfaceTintColor: Colors.white,
-        elevation: 10,
-        shadowColor: Colors.black,
-      ),
-    );
+        appBar: BaseAppBar(AppBar(), '로그인', false, false, false),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                child: Text(
+                  '어서오세요!',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                ),
+                margin: const EdgeInsets.fromLTRB(46, 46, 0, 10)),
+            Container(
+              child: Text(
+                '롤링마인드는 로그인이 필요한 서비스 입니다.',
+                style: TextStyle(
+                    fontWeight: FontWeight.w500, fontSize: 14, color: darkGrey),
+              ),
+              margin: const EdgeInsets.fromLTRB(46, 0, 0, 0),
+            ),
+            TextFieldArea('아이디', 35, 79, 35, 0),
+            TextFieldArea('비밀번호', 35, 35, 35, 0)
+          ],
+        ));
   }
 }
---
+++ lib/utilities/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+const Color darkGrey = const Color(0xff8A8A8A);
+const Color lightGrey = const Color(0xffD9D9D9);
+const Color pink = const Color(0xffFD928B);

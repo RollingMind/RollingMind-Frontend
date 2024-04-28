c397f5aafc23b438f54ca3930f59c2d48c43471b
MsEmily1020 <peace10200khs@gmail.com>
2024-04-28 15:44:38 +0900

    Design :: 아이디 찾기 - 이메일 입력창 margin 조정

--- lib/pages/auth/find_id_page.dart
+++ lib/pages/auth/find_id_page.dart
@@ -42,42 +42,48 @@ class FindIdPage extends StatelessWidget {
                 const SizedBox(
                   height: 55,
                 ),
-                const Text(
-                  '이메일',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(5, 0, 0, 4),
+                  child: Text(
+                    '이메일',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14
+                    ),
                   ),
                 ),
                 const SizedBox(
                   height: 2,
                 ),
-                Form(
-                  autovalidateMode: AutovalidateMode.always,
-                  key: formKeys[0],
-                  child: TextFormField(
-                    validator: (String? value) {
-                      if(value?.isEmpty ?? true) {
-                        return '빈칸입니다. 이메일을 입력해주세요.';
-                      } else if(!isEmail(value!)) { 
-                        return '이메일을 확인해주세요.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      fillColor: AppColor.grey01F0,
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(10)
+                SizedBox(
+                  width: 325,
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    key: formKeys[0],
+                    child: TextFormField(
+                      validator: (String? value) {
+                        if(value?.isEmpty ?? true) {
+                          return '빈칸입니다. 이메일을 입력해주세요.';
+                        } else if(!isEmail(value!)) { 
+                          return '이메일을 확인해주세요.';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        fillColor: AppColor.grey01F0,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        helperText: '*입력하신 이메일은 본인 확인의 용도 외에는\n사용되지 않습니다.',
+                        helperStyle: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: AppColor.green
+                        ),
+                        contentPadding: EdgeInsets.all(13),
                       ),
-                      helperText: '*입력하신 이메일은 본인 확인의 용도 외에는\n사용되지 않습니다.',
-                      helperStyle: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12,
-                        color: AppColor.green
-                      ),
-                      contentPadding: EdgeInsets.all(13),
-                    ),
+                    )
                   )
                 )
               ]
